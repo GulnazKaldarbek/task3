b73a072c0c35b9f5f6f56560ef48ea591ea8b55c
GulnazKaldarbek <gulnaz.kaldarber@narxoz.kz>
2024-02-22 10:43:38 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./scss/style.scss";
+import bg from "./assets/bg.jpg";
+import cup from "./assets/cup.png";
+import heart from "./assets/heart.png";
+import shoe from "./assets/shoe.png";
+
+// const bgImage = document.getElementById("bgImage");
+// bgImage.src = bg;
+
+const cupImage = document.getElementById("cupImage");
+cupImage.src = cup;
+
+const heartImage = document.getElementById("heartImage");
+heartImage.src = heart;
+
+const shoeImage = document.getElementById("shoeImage");
+shoeImage.src = shoe;
